0baa30ade828b1a257ee8f8f5be379f8269be712
maxtwen <maxtwen@yandex.ru>
2016-10-04 21:12:28 +0300

    bootloader

--- makeos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-nasm boot_sect.asm -f bin -o boot_sect.bin
-nasm kernel_entry.asm -f elf64 -o kernel_entry.o
-docker run -v $PWD:/tmp -w /tmp gcc gcc -ffreestanding -c kernel.c -o kernel.o
-docker run -v $PWD:/tmp -w /tmp gcc gcc -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o
-cat boot_sect.bin kernel.bin > os-image
-qemu-system-i386 os-image
